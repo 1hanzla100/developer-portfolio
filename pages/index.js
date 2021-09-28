e3ad520106e81b4cc1cc47b7817d0e187793ba98
Hanzla <hanzla.tauqeer123@gmail.com>
2021-09-28 06:36:40 -0700

    SEO

--- pages/index.js
+++ pages/index.js
@@ -29,10 +29,10 @@ export default function Home({ githubProfileData }) {
 					name="keywords"
 					content="Hanzla, Hanzla Tauqeer, @1hanzla100, 1hanzla100, Portfolio, Hanzla Portfolio , Hanzla Tauqeer Portfolio, web developer, full stack, full stack web developer, mobile app developer, android developer, django, flask, django rest framework, nodejs , expressjs, reactjs , contextapi, redux, flutter"
 				/>
-				<link rel="canonical" href="https://hanzla.netlify.app" />
+				<link rel="canonical" href="https://developer-portfolio-1hanzla100.vercel.app" />
 				{/* Open Graph / Facebook */}
 				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://hanzla.netlify.app" />
+				<meta property="og:url" content="https://developer-portfolio-1hanzla100.vercel.app/" />
 				<meta property="og:title" content="Hanzla's Portfolio" />
 				<meta
 					property="og:description"
@@ -47,7 +47,7 @@ export default function Home({ githubProfileData }) {
 				<meta property="twitter:card" content="summary_large_image" />
 				<meta
 					property="twitter:url"
-					content="https://hanzla.netlify.app"
+					content="https://developer-portfolio-1hanzla100.vercel.app/"
 				/>
 				<meta property="twitter:title" content="Hanzla's Portfolio" />
 				<meta
@@ -58,6 +58,7 @@ export default function Home({ githubProfileData }) {
 					property="twitter:image"
 					content="https://avatars3.githubusercontent.com/u/59178380?v=4"
 				/>
+				<meta name="robots" content="Index"/>
 				<link rel="manifest" href="/manifest.json" />
 				{/* Favicon */}
 				<link

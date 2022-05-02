1de105f685b2d4d97b96c389bab4930b2e9bd5ec
Hanzla <hanzla.tauqeer123@gmail.com>
2022-05-02 15:21:44 +0500

    Update Skills.jsx

--- containers/Skills.jsx
+++ containers/Skills.jsx
@@ -13,7 +13,14 @@ const Skills = () => {
 			{skillsSection.data.map((section, index) => {
 				return (
 					<Row className="my-5" key={index}>
-						<Col lg="6">
+						<Col lg="6" className="order-2 order-lg-1">
+							<Fade left duration={2000}>
+								<DisplayLottie
+									animationPath={section.lottieAnimationFile}
+								/>
+							</Fade>
+						</Col>
+						<Col lg="6" className="order-1 order-lg-2">
 							<Fade right duration={2000}>
 								<h3 className="h3 mb-2">{section.title}</h3>
 								<div className="d-flex justify-content-center flex-wrap mb-2">
@@ -47,13 +54,6 @@ const Skills = () => {
 										return <p key={i}>{skill}</p>;
 									})}
 								</div>
-							</Fade>
-						</Col>
-						<Col lg="6">
-							<Fade left duration={2000}>
-								<DisplayLottie
-									animationPath={section.lottieAnimationFile}
-								/>
 							</Fade>
 						</Col>
 					</Row>

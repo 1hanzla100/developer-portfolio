c47f594e07af80d4f4f05d990c50a3bc6aba5a4d
Hanzla <hanzla.tauqeer123@gmail.com>
2022-09-11 18:50:42 +0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -16,11 +16,12 @@ export const openSource = {
 export const contact = {};
 
 export const socialLinks = {
-	facebook: "https://www.facebook.com/1hanzla100",
-	instagram: "https://www.instagram.com/__hanzla100",
-	twitter: "https://twitter.com/1hanzla100",
-	github: "https://github.com/1hanzla100",
+	url: "https://1hanzla100.github.io/",
 	linkedin: "https://www.linkedin.com/in/1hanzla100/",
+	github: "https://github.com/1hanzla100",
+	instagram: "https://www.instagram.com/__hanzla100",
+	facebook: "https://www.facebook.com/1hanzla100",
+	twitter: "https://twitter.com/1hanzla100",
 };
 
 export const skillsSection = {

d20c7ffd8b328aeac146f957f6ef2c53408d64c8
amirrezaDev1378 <rezaamir731@gmail.com>
2022-09-20 18:39:08 +0430

    added dark theme config

---
+++ styles/theme/darkTheme/index.js
@@ -0,0 +1,11 @@
+export const darkTheme = {
+    textColor: "textColorDarkTheme",
+    oppositeTextColor: "oppositeTextColorDarkTheme",
+    oppositeTextColorLighter: "text-light",
+
+    buttonColor: "btn-darker",
+    bgGradient: "bg-gradient-gray",
+    globalBg: "bg-darker",
+
+
+}

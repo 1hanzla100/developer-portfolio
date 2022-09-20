5105920f29eab9edd30899be50beb0d9b6e78e16
amirrezaDev1378 <rezaamir731@gmail.com>
2022-09-20 18:39:14 +0430

    added light theme config

---
+++ styles/theme/lightTheme/index.js
@@ -0,0 +1,8 @@
+export const lightTheme = {
+    textColor: "textColorLightTheme",
+    oppositeTextColor: "oppositeTextColorLightTheme",
+    oppositeTextColorLighter: "text-default",
+    buttonColor: "btn-white",
+    bgGradient: "bg-gradient-info",
+    globalBg: "bg-white",
+}

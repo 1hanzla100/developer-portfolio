e8bb27b59ecacf44665a64da454c42bcee66f783
amirrezaDev1378 <rezaamir731@gmail.com>
2022-09-20 18:37:08 +0430

    added themeContext in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,22 @@ import "../styles/argon-design-system-react.css";
 import "../styles/styles.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
+import {defaultValues, ThemeContext} from "../styles/theme/theme";
+import {useState} from "react";
 
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+function MyApp({Component, pageProps}) {
+    const [theme, setTheme] = useState(defaultValues.theme);
+    const [mode, setMode] = useState(defaultValues.mode);
+    const initialValues = {
+        mode,
+        theme,
+        setTheme,
+        setMode
+    }
+
+    return (<ThemeContext.Provider value={initialValues}>
+        <Component {...pageProps} />
+    </ThemeContext.Provider>);
 }
 
 export default MyApp;

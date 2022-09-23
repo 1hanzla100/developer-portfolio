77788ebca36918e6241bdbb8cad0b4b1c18dd1f9
amirrezaDev1378 <rezaamir731@gmail.com>
2022-09-23 10:34:49 +0330

    added icon from iconify for theme switch button.

--- components/switchTheme.jsx
+++ components/switchTheme.jsx
@@ -1,21 +1,29 @@
 import React, {useContext} from 'react';
 import {Button} from "reactstrap";
 import {ThemeContext, THEMES} from "../styles/theme/theme";
+import {Icon, addIcon, addCollection} from '@iconify/react/offline';
+
+import Light from '@iconify-icons/bi/lightbulb';
+import LightFill from '@iconify-icons/bi/lightbulb-fill';
 
 const SwitchTheme = () => {
-    const {setMode , setTheme , mode} = useContext(ThemeContext);
+    const {setMode, setTheme, mode} = useContext(ThemeContext);
     const switchTheme = () => {
-        setMode((prev)=>{
+        setMode((prev) => {
             return prev === "light" ? "dark" : "light";
         });
-        setTheme((prev)=>{
+        setTheme((prev) => {
             return prev === THEMES.light ? THEMES.dark : THEMES.light;
         });
     }
     return (
         <div>
-            <Button style={{zIndex:5}} onClick={switchTheme}>
-                Switch Theme
+            <Button className={mode === "light" ? "switchThemeLight" : "switchThemeDark"} style={{zIndex: 5}} onClick={switchTheme}>
+                <Icon icon={
+                    mode === "light" ? Light : LightFill
+                }
+                      width={35}
+                />
             </Button>
         </div>
     );

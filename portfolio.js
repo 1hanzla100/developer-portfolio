7b7374d0dfadc02b56ff2e8a8b801376a1f491a0
aakash193 <89975560+aakash193@users.noreply.github.com>
2022-12-09 13:41:40 +0545

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,8 @@
 import emoji from "react-easy-emoji";
 
 export const greetings = {
-	name: "Hanzla Tauqeer",
-	title: "Hi all, I'm Hanzla",
+	name: "Aakash Khanal",
+	title: "Hi all, I'm Aakash",
 	description:
 		"I'm passionate Full Stack web developer having an experience of web applications with Python, Django, React.js, Next.js and Blockchain development on Ethereum, Solidity, Web3.js, Moralis, and Brownie Framework.",
 	resumeLink:
